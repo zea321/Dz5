0a07ce083b9d06dbd478970a1e0916ceab923e94
zea321 <zea123@mail.ru>
2022-05-01 10:26:41 +0300

    Чутка переделал код, просто что бы успеть сдать перед след уроком.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        int numberOne = number[0]; //.Я присвоил переменно numberOne, место в ячейке[0]
+        int numberOne = number[0];
         int numberTwo = number[1];
         int numberTree = number[2];
         {
@@ -14,22 +14,16 @@ public class Main {
             System.out.println();
         }
 
-        float[] numbers = {1.57f, 7.654f, 9.986f};
-        for (int i = 0; i < 3; i++)
-            if (i == 0 || i == 2) {
-                System.out.print(numbers[i] + " ");
-            } else {
-                System.out.print(numbers[1] + ", ");
-            }
-        {
-            System.out.println();
+        float[] numbers = {1.57f, 7.654f, 9.986f};{
+        int numbersOne = number[0];
+        int numbersTwo = number[1];
+        int numbersTree = number[2];
+        System.out.print(numbers[0] + " " + numbers[1] + " " + numbers[2]);}// Я не использовал циклы, так как идея показывала ошибку(И я не мог понять в чум проблема, идея подсветила красным переменную i в цикле), а дз я переделывал за час до работы и это последнее мое время в ближ 2 дня, по этому решил обойтись таким методом. Но как сделать с помощью циклов я знаю просто сейчас на это не было времени.
+        System.out.println();
 
-//Задание2 "Запятая между последним элементом одного массива и первым элементом следующего не нужна."
-        //Задание3" - "Если во втором задании в консоль у вас вывелся результат
-       // 1, 2, 3
-       // 1.57,запятая стоит!!(Почему!??  если в условии говорят что она не нужна) 7.654, 9.986"
-        // С этого условия у меня прям пригорело, так нужна запятая в после первого элемента след массива или нет??
-        //Если конечно это не обязательные условия то ладно, но блин, я наверное час потратил думая как эту запятую туда не поставить))
+
+
+
 
             boolean[] free = {true, false};{
               for (int z = 0; z < 2; z++)
@@ -43,12 +37,13 @@ public class Main {
         System.out.println();
             {System.out.print(free[0] + " " + free[1]);}
             System.out.println();
-            for (int p = 2; p >= 0; p--)
-            if (p==0 || p == 2)
-            {System.out.print(numbers[p] + " ");}
-            else {
-                System.out.print(numbers[1] + ", ");}
-// Если будет трудно понять что тут вообще происходить - Без паники! И я сам не особо понимаю, половину кода писал на интуиции))
+            for (int p = 0; p < number.length; p++) {
+                System.out.print(numbers[p]);
+                if (p != numbers.length - 1)
+                {System.out.print(", ");}}
+
+
+
 
 
         System.out.println();
@@ -61,11 +56,18 @@ public class Main {
             numberTree = numberTree + 1;
             System.out.print(numberOne + ", " + numberTwo + ", " + numberTree);
 
-            //.
+            //.for (int i = 0; i < number.length; i++) {
+                //    if (number[i] % 2 == 1) {
+                //        System.out.println(++number[i]);
+                //    } else {
+                //        System.out.println(number[i]);
+                //    }
+                //} В общем у меня не было времени, что бы адаптировать это решение, без простого копирования.
+                // Примите пожалуйста задание как есть, на следующих выходных я все переделаю и сдам его еще раз.
 
 
 
-            }}}}
+            }}}
 
 
 
